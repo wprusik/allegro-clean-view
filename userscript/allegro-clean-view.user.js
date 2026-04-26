5c720be8b226fe27617b9ffe255cb9f00e97e46e
Wojciech Prusik <wojciech.prusik@int.pl>
2026-04-26 11:56:29 +0200

    RELEASE VERSION 1.2.2

--- userscript/allegro-clean-view.user.js
+++ userscript/allegro-clean-view.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Allegro Clean View (UserScript)
 // @namespace    https://github.com/wprusik/allegro-clean-view
-// @version      1.2.1
+// @version      1.2.2
 // @description  Przywraca bardziej czytelny widok Allegro: parametry/opis oferty + usuwanie rekomendacji.
 // @author       allegro-clean-view
 // @homepageURL  https://github.com/wprusik/allegro-clean-view
